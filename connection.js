5bf19fe08d99ed53898e38d7b256c7e83a849884
MrAgungS <agung.worksss@gmail.com>
2025-12-10 16:36:48 +0700

    perubahan

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const db = mysql.createConnection({
     host: "127.0.0.1",
-    user:"root",
+    user: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: "coba-express",
 })
